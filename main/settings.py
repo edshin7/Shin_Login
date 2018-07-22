2ceae98fdd1151cfd60f1d5520aa4403f1a0b26d
Edward Shin <edshin7@gmail.com>
2018-07-22 10:08:36 -0700

    "practice update"

--- main/settings.py
+++ main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uz_wd1^38k9k3w=x_^^yyq*o@@w2+!8r#03&+k_8=#f_b3$ic@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['packet_write_wait: Connection to 18.222.77.99 port 22: Broken pipe']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
